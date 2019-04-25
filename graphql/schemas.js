f132a8e45bf345379ed4bba853b05465fd9416a4
Erik Ståhl <e.stahl95@gmail.com>
2019-04-25 16:22:07 +0200

    Added an early version of auction model

--- graphql/schemas.js
+++ graphql/schemas.js
@@ -9,6 +9,7 @@ import {
 } from 'graphql';
 import GraphQLDate from 'graphql-date';
 import UserModel from '../models/User';
+import AuctionModel from '../models/Auction';
 
 var userType = new GraphQLObjectType({
   name: 'User',
@@ -33,10 +34,43 @@ var userType = new GraphQLObjectType({
   }
 });
 
+var auctionType = new GraphQLObjectType({
+  name: 'Auction',
+  fields: function() {
+    return {
+      _id: {
+        type: GraphQLString
+      },
+      seller: {
+        type: userType
+      },
+      title: {
+        type: GraphQLString
+      },
+      description: {
+        type: GraphQLString
+      },
+      items: {
+        type: GraphQLString
+      },
+      number_of_items: {
+        type: GraphQLInt
+      },
+      start_time: {
+        type: GraphQLDate
+      },
+      created_at: {
+        type: GraphQLDate
+      }
+    };
+  }
+});
+
 var queryType = new GraphQLObjectType({
   name: 'Query',
   fields: function() {
     return {
+      // User
       users: {
         type: new GraphQLList(userType),
         resolve: function() {
@@ -62,6 +96,34 @@ var queryType = new GraphQLObjectType({
           }
           return userDetails;
         }
+      },
+
+      // Auction
+      auctions: {
+        type: new GraphQLList(auctionType),
+        resolve: function() {
+          const auctions = AuctionModel.find().exec();
+          if (!auctions) {
+            throw new Error('Error');
+          }
+          return auctions;
+        }
+      },
+      auction: {
+        type: auctionType,
+        args: {
+          id: {
+            name: '_id',
+            type: GraphQLString
+          }
+        },
+        resolve: function(root, params) {
+          const auctionDetails = AuctionModel.findById(params.id).exec();
+          if (!auctionDetails) {
+            throw new Error('Error');
+          }
+          return auctionDetails;
+        }
       }
     };
   }
@@ -71,6 +133,7 @@ var mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: function() {
     return {
+      // User
       addUser: {
         type: userType,
         args: {
@@ -137,6 +200,98 @@ var mutation = new GraphQLObjectType({
             throw new Error('Error');
           }
           return remUser;
+        }
+      },
+
+      // Auction
+      addAuction: {
+        type: auctionType,
+        args: {
+          /* 
+          seller: {
+            type: new GraphQLNonNull(userType)
+          }, */
+          title: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          description: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          items: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          number_of_items: {
+            type: new GraphQLNonNull(GraphQLInt)
+          } /* ,
+          start_time: {
+            type: new GraphQLNonNull(GraphQLDate)
+          } */
+        },
+        resolve: function(root, params) {
+          const auctionModel = new AuctionModel(params);
+          const newAuction = auctionModel.save();
+          if (!newAuction) {
+            throw new Error('Error');
+          }
+          return newAuction;
+        }
+      },
+      updateAuction: {
+        type: auctionType,
+        args: {
+          id: {
+            name: 'id',
+            type: new GraphQLNonNull(GraphQLString)
+          } /* 
+          seller: {
+            type: new GraphQLNonNull(userType)
+          }, */,
+          title: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          description: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          items: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          number_of_items: {
+            type: new GraphQLNonNull(GraphQLInt)
+          } /* ,
+          start_time: {
+            type: new GraphQLNonNull(GraphQLDate)
+          } */
+        },
+        resolve(root, params) {
+          return AuctionModel.findByIdAndUpdate(
+            params.id,
+            {
+              seller: params.seller,
+              title: params.title,
+              description: params.description,
+              items: params.items,
+              number_of_items: params.number_of_items,
+              start_time: params.start_time
+            },
+            function(err) {
+              if (err) return next(err);
+            }
+          );
+        }
+      },
+      removeAuction: {
+        type: auctionType,
+        args: {
+          id: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve(root, params) {
+          const remAuction = AuctionModel.findByIdAndRemove(params.id).exec();
+          if (!remAuction) {
+            throw new Error('Error');
+          }
+          return remAuction;
         }
       }
     };
