8214dfab49b58406b92bf49a8e0d5ae71b5a5b5e
Erik Ståhl <e.stahl95@gmail.com>
2019-04-29 20:00:51 +0200

    Create now works for users

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -4,6 +4,18 @@ export default {
   Query: {
     users: (root, args, context, info) => {
       return User.find({});
+    },
+    user: async (root, args, context, info) => {
+      return User.findById(args.id);
+    }
+  },
+  Mutation: {
+    signUp: async (root, args, { req }, info) => {
+      const user = await User.create(args);
+
+      //req.session.userId = user.id;
+
+      return user;
     }
   }
 };
