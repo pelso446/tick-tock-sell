6d69bf2cd60f078981c86c9e2fd2ca7d60602fe6
Pelle Söderberg <pelso446@student.liu.se>
2019-05-03 15:20:28 +0200

    Apollo setup for query and mutations done

--- client/src/components/AuctionForm.js
+++ client/src/components/AuctionForm.js
@@ -9,60 +9,75 @@ const ADD_AUCTION = gql`
     $title: String!
     $description: String!
     $startTime: String!
+    $items: [itemInput!]!
   ) {
     createAuction(
       sellerID: $sellerID
       title: $title
       description: $description
       startTime: $startTime
+      items: $items
     ) {
       id
     }
   }
 `;
-
 class Create extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [{title: "", price: ""}]
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-      
-      addClick(){
-        this.setState(prevState => ({ 
-            items: [...prevState.items, { title: "", price: "" }]
-        }))
-      }
-      
-      createUI(){
-         return this.state.items.map((el, i) => (
-           <div key={i}>
-              <input placeholder="Title" name="title" value={el.title ||''} onChange={this.handleChange.bind(this, i)} />
-              <input placeholder="Price" name="price" value={el.price ||''} onChange={this.handleChange.bind(this, i)} />
-              <input type='button' value='remove' onClick={this.removeClick.bind(this, i)}/>
-           </div>          
-         ))
-      }
-      
-      handleChange(i, e) {
-         const { title, value } = e.target;
-         let items = [...this.state.items];
-         items[i] = {...items[i], [title]: value};
-         this.setState({ items });
-      }
-      
-      removeClick(i){
-         let items = [...this.state.items];
-         items.splice(i, 1);
-         this.setState({ items });
-      }
-      
-      handleSubmit(event) {
-        alert('A name was submitted: ' + JSON.stringify(this.state.items));
-        event.preventDefault();
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [{ itemTitle: '', itemDescription: '' }]
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  addClick() {
+    this.setState(prevState => ({
+      items: [...prevState.items, { itemTitle: '', itemDescription: '' }]
+    }));
+  }
+
+  createUI() {
+    return this.state.items.map((el, i) => (
+      <div key={i}>
+        <input
+          placeholder='Title'
+          name='itemTitle'
+          value={el.itemTitle|| ''}
+          onChange={this.handleChange.bind(this, i)}
+        />
+        <input
+          placeholder='Price'
+          name='itemDescription'
+          value={el.itemDescription || ''}
+          onChange={this.handleChange.bind(this, i)}
+        />
+        <input
+          type='button'
+          value='remove'
+          onClick={this.removeClick.bind(this, i)}
+        />
+      </div>
+    ));
+  }
+
+  handleChange(i, e) {
+    const { name, value } = e.target;
+    let items = [...this.state.items];
+    items[i] = { ...items[i], [name]: value };
+    this.setState({ items });
+  }
+
+  removeClick(i) {
+    let items = [...this.state.items];
+    items.splice(i, 1);
+    this.setState({ items });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + JSON.stringify(this.state.items));
+    event.preventDefault();
+  }
 
   render() {
     let title, description, startTime;
@@ -91,7 +106,8 @@ class Create extends Component {
                         sellerID: '5ccabaf90ae5a1153a0f5e14',
                         title: title.value,
                         description: description.value,
-                        startTime: startTime.value
+                        startTime: startTime.value,
+                        items: this.state.items
                       }
                     });
                     title.value = '';
