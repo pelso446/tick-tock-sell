167e52939b24e03d4d9f959e27f44df1ee61b29a
Pelle Söderberg <pelso446@student.liu.se>
2019-05-06 14:16:21 +0200

    Backend auth under construction

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,4 +1,7 @@
 import { User, Auction } from '../../models';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { APP_SECRET, getUserId } from '../utils';
 
 export default {
   Query: {
@@ -11,8 +14,38 @@ export default {
   },
   Mutation: {
     signUp: async (root, args, { req }, info) => {
-      const user = await User.create(args);
-      return user;
+      const { name, email } = args;
+      const password = await bcrypt.hash(args.password, 10);
+      const user = await User.create({
+        name,
+        email,
+        password
+      });
+      console.log(JSON.stringify(user));
+
+      const token = jwt.sign({ userId: user._id }, APP_SECRET);
+      return { token, user };
+    },
+    signIn: async (root, args, { req }, info) => {
+      const user = await User.find({ email: args.email });
+      if (!user) {
+        throw new Error('No such user found');
+      }
+      console.log('user: ' + typeof user);
+
+      console.log('arg password: ' + args.password);
+      console.log('user password: ' + user[email];
+
+      const valid = await bcrypt.compare(args.password, user.password);
+      console.log('valid: ' + valid);
+
+      if (!valid) {
+        throw new Error('Invalid password');
+      }
+      const token = jwt.sign({ userId: user._id }, APP_SECRET);
+      console.log('token: ' + token);
+
+      return { token, user };
     }
   },
   User: {
