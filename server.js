16d72dd898c70337a1af0202f41db8a90345c1b6
Erik Ståhl <e.stahl95@gmail.com>
2019-05-08 10:00:26 +0200

    Heroku update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (IN_PROD) {
   app.use(express.static('public'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 5000;
